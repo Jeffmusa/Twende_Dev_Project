97881cd4ad754cfd01ba02da912b4c4515d76327
Jeff Musa <jeffmusa@gmail.com>
2019-03-07 14:04:43 +0300

    latest commit

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_photos,process_results
+# from ..requests import get_photos,process_results
+import requests
+import json
 
 # Views
 @main.route('/')
@@ -9,10 +11,22 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    #getting photos
-    photos = get_photos()
-    # print('photos')
+    r = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1337&api_key=lhryAfcIDBcWy2tlslGe7EYppRa44WueiXGJ1q8U').json()
+    t = r['photos']
+        
+    camera = r['photos'][0]['camera']['name']
+    rover = r['photos'][0]['rover']
+    d = rover.keys()
+    print(d)
+    # for k,v in rover.items():
+    #     # print(k)
+    # # for key, value in s.items():
+    #     # name = data.get('name')
+
+    #     return (k,v)
+
+
     
-    return render_template('index.html',photos = photos)
+    return render_template('index.html' ,t = t)
 
 
--- app/requests.py
+++ app/requests.py
@@ -1,61 +1,74 @@
+'''
+This folder will make the app load slow when used
+Instead, what should be here is at the views function
 
-from . import main
-import urllib.request,json
-from .models import Rover
-
-
-api_key = None
-
-base_url = None
-
-photos_url = None
+'''
 
 
-def configure_request(app):
-    global api_key,base_url,photos_url
-    api_key = app.config['ROVER_API_KEY']
-    base_url = app.config['ROVER_API_SOURCES_URL']
-
-def get_photos():
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_photos_url = base_url.format(api_key)
-
-    with urllib.request.urlopen(get_photos_url) as url:
-        get_photos_data = url.read()
-        get_photos_response = json.loads(get_photos_data)
-
-        photos_results = None
-
-        if get_photos_response['photos']:
-            photos_results_list = get_photos_response['photos']
-            # print (photos_results_list)
-            photos_results = process_results(photos_results_list)
+# from . import main
+# import urllib.request,json
+# from .models import Rover
+# import requests
+# import json
 
 
-    return photos_results
+# api_key = None
+
+# base_url = None
+
+# photos_url = None
+
+
+# def configure_request(app):
+#     global api_key,base_url,photos_url
+#     api_key = app.config['ROVER_API_KEY']
+#     base_url = app.config['ROVER_API_SOURCES_URL']
+
+# def get_photos():
+#     '''
+#     Function that gets the json response to our url request
+#     '''
+#     get_photos_url = base_url.format(api_key)
+
+#     with urllib.request.urlopen(get_photos_url) as url:
+#         get_photos_data = url.read()
+#         get_photos_response = json.loads(get_photos_data)
+
+#         photos_results = None
+
+#         if get_photos_response['photos']:
+#             photos_results_list = get_photos_response['photos']
+#             # print (photos_results_list)
+#             photos_results = process_results(photos_results_list)
+
+
+#     return photos_results
 
 
 
-def process_results(photos_list):
-    '''
-    Function  that processes the news result and transform them to a list of Objects
+# def process_results(photos_list):
+#     '''
+#     Function  that processes the news result and transform them to a list of Objects
 
-    Args:
-        photos_list: A list of dictionaries that contain photos details
+#     Args:
+#         photos_list: A list of dictionaries that contain photos details
 
-    Returns :
-        photos_results: A list of photos objects
-    '''
-    photos_results = []
-    for photo_item in photos_list:
-        date = photo_item.get('earth_date')
-        img_src = photo_item.get('img_src')
+#     Returns :
+#         photos_results: A list of photos objects
+#     '''
+#     # photos_results = []
+#     # for photo_item in photos_list:
+#     #     date = photo_item.get('earth_date')
+#     #     img_src = photo_item.get('img_src')
       
         
-        photos_object = Rover(date,img_src)
-        photos_results.append(photos_object)
-        print (photos_results)
+#     #     # photos_object = Rover(date,img_src)
+#     #     # photos_results.append(photos_object)
+#     #     print (date)
+
+#     r = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1337&api_key=lhryAfcIDBcWy2tlslGe7EYppRa44WueiXGJ1q8U').json()
+#     s = r['photos'][0]['camera']
+
+
         
-    return photos_results    
+#     return s
